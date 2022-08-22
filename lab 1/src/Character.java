cbcf21f69b4e020cd0e804ec9178990c7a2b7f3b
Jhon Carlo Palatino <111470308+Carlo06@users.noreply.github.com>
2022-08-22 11:05:20 +0800

    Add files via upload

---
+++ lab 1/src/Character.java
@@ -0,0 +1,34 @@
+public class Character {
+    int strength;
+    int agility;
+    int intelligence;
+    String name;
+
+    public void sayMyName() {
+        System.out.println("Hello I am" +  name );      
+
+    }
+    
+    public void sayMyStrength(){
+        System.out.println("my strength is" +  strength );
+
+
+    }
+
+    public void sayMyAgility(){
+        System.out.println("my agility is" +  agility );
+
+
+
+    }
+
+    public void sayMyIntelligence(){
+        System.out.println("my intelligence is" +  intelligence );
+
+
+    }
+
+
+
+
+}
---
+++ lab 1/src/math.java
@@ -0,0 +1,23 @@
+public class math {
+    public static void main(String[] args) {
+        int x = 20;
+        int y = 60;
+        int z = 190;
+        
+
+        System.out.println(x - y * z + y / z);
+
+
+
+
+
+    }
+        
+    
+
+
+        
+        
+    
+    
+}
