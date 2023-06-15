2fdf0e23c33625cda3b6420a059a3dc645aabf87
Jhon Carlo Palatino <111470308+Carlo06@users.noreply.github.com>
2023-06-16 00:08:59 +0800

    Add files via upload

---
+++ JAACK/src/controller/WomenCheckoutController.java
@@ -0,0 +1,186 @@
+package controller;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+import javafx.scene.Node;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+
+public class WomenCheckoutController implements Initializable {
+
+    @FXML
+    Pane pane1, pane2, pane3, pane4, pane5, pane6, pane7, pane8;
+
+    @FXML
+    VBox myvbox;
+
+    @FXML
+    Label name1, name2, name3, name4, name5, name6, name7, name8,  price1, price2, price3, price4, price5, price6, price7, price8, total;
+
+    @FXML
+    ImageView img1, img2, img3, img4, img5, img6, img7, img8;
+
+    @FXML
+    private ChoiceBox<String> choicebox1, choicebox2, choicebox3, choicebox4, choicebox5, choicebox6, choicebox7, choicebox8;
+
+    private String[] quantity = { "1", "2", "3" };
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        name1.setText(WomenController.Bcap.getProductName());
+        price1.setText(Double.toString(WomenController.Bcap.getProductPrice()));
+        Image beigeCap = new Image(WomenController.Bcap.getProductImage());
+        img1.setImage(beigeCap);
+
+        name2.setText(WomenController.mcoq.getProductName());
+        price2.setText(Double.toString(WomenController.mcoq.getProductPrice()));
+        Image MiniCoquette = new Image(WomenController.mcoq.getProductImage());
+        img2.setImage(MiniCoquette);
+
+        name3.setText(WomenController.Shoes.getProductName());
+        price3.setText(Double.toString(WomenController.Shoes.getProductPrice()));
+        Image BeigeShoes = new Image(WomenController.Shoes.getProductImage());
+        img3.setImage(BeigeShoes);
+
+        name4.setText(WomenController.cap.getProductName());
+        price4.setText(Double.toString(WomenController.cap.getProductPrice()));
+        Image Bucket = new Image(WomenController.cap.getProductImage());
+        img4.setImage(Bucket);
+
+        // Set default quantities in choicebox to 1
+        choicebox1.setValue("1");
+        choicebox2.setValue("1");
+        choicebox3.setValue("1");
+        choicebox4.setValue("1");
+
+        // Insert quantity array to choicebox
+        choicebox1.getItems().addAll(quantity);
+        choicebox2.getItems().addAll(quantity);
+        choicebox3.getItems().addAll(quantity);
+        choicebox4.getItems().addAll(quantity);
+
+        // Add event handler on all choiceboxes
+        choicebox1.setOnAction(this::computeTotal);
+        choicebox2.setOnAction(this::computeTotal);
+        choicebox3.setOnAction(this::computeTotal);
+        choicebox4.setOnAction(this::computeTotal);
+
+        // Set total initial amount
+        double totalInitialAmount = 0.00;
+        if (WomenController.mcoq.getProductStatus() || WomenController.Bcap.getProductStatus() || WomenController.Shoes.getProductStatus()) {
+            totalInitialAmount = Double.parseDouble(choicebox1.getValue()) * WomenController.Bcap.getProductPrice() +
+            +Double.parseDouble(choicebox2.getValue()) * WomenController.mcoq.getProductPrice()
+            + Double.parseDouble(choicebox3.getValue()) * WomenController.Shoes.getProductPrice();
+        }
+
+        if (WomenController.mcoq.getProductStatus() || WomenController.Bcap.getProductStatus() || WomenController.Shoes.getProductStatus() || WomenController.cap.getProductStatus() ) {
+            totalInitialAmount = Double.parseDouble(choicebox1.getValue()) * WomenController.Bcap.getProductPrice() +
+            +Double.parseDouble(choicebox2.getValue()) * WomenController.mcoq.getProductPrice()
+            + Double.parseDouble(choicebox3.getValue()) * WomenController.Shoes.getProductPrice() +
+            Double.parseDouble(choicebox4.getValue()) * WomenController.cap.getProductPrice() ;
+        }
+     
+        // Display total initial amount in total label
+        total.setText(Double.toString(totalInitialAmount));
+    }
+
+    public void addItem(Pane pane) {
+        myvbox.getChildren().add(pane);
+    }
+
+    public void computeTotal(ActionEvent event) {
+
+        double totalAmount = 0;
+        double item1Amount = 0;
+        double item2Amount = 0;
+        double item3Amount = 0;
+        double item4Amount = 0;
+        double item5Amount = 0;
+        double item6Amount = 0;
+        double item7Amount = 0;
+        double item8Amount = 0;
+
+
+        ChoiceBox source = (ChoiceBox) event.getSource();
+
+        // If product is chosen, compute item amount
+        if (WomenController.Bcap.getProductStatus()) {
+
+            double qty = Double.parseDouble(choicebox1.getValue());
+            WomenController.Bcap.setProductQuantity(qty);
+            item1Amount = WomenController.Bcap.getProductPrice() * qty;
+            
+            if (source == choicebox1) {
+                item1Amount = WomenController.Bcap.getProductPrice() * qty;
+            }
+        }
+
+        if (WomenController.mcoq.getProductStatus()) {
+
+            double qty = Double.parseDouble(choicebox2.getValue());
+            WomenController.mcoq.setProductQuantity(qty);
+            item2Amount = WomenController.mcoq.getProductPrice() * qty;
+            if (source == choicebox2) {
+                item2Amount = WomenController.mcoq.getProductPrice() * qty;
+            }
+        }
+
+        if (WomenController.Shoes.getProductStatus()) {
+
+            double qty = Double.parseDouble(choicebox3.getValue());
+            WomenController.Shoes.setProductQuantity(qty);
+            item3Amount = WomenController.Shoes.getProductPrice() * qty;
+
+            if (source == choicebox3) {
+                item3Amount = WomenController.Shoes.getProductPrice() * qty;
+            }
+        }
+
+        if (WomenController.cap.getProductStatus()) {
+
+            double qty = Double.parseDouble(choicebox4.getValue());
+            WomenController.cap.setProductQuantity(qty);
+            item4Amount = WomenController.cap.getProductPrice() * qty;
+
+            if (source == choicebox4) {
+                item4Amount = WomenController.cap.getProductPrice() * qty;
+            }
+        }
+
+
+
+        // Compute total amount for all items chosen
+        totalAmount = item1Amount + item2Amount + item3Amount;
+        
+        // Display total amount in total label
+        total.setText(Double.toString(totalAmount));
+    }
+
+    public void switchToReceipt(ActionEvent event) throws IOException {
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/Order.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+        
+    }
+}
---
+++ JAACK/src/controller/WomenController.java
@@ -0,0 +1,191 @@
+package controller;
+
+import model.*;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import alert.AlertMaker;
+
+public class WomenController implements Initializable {
+
+    @FXML
+    Label name1, name2, name3,name4,name5,name6,name7,name8, price1, price2, price3, price4, price5, price6, price7, price8;
+
+    @FXML
+    ImageView img1, img2, img3, img4, img5, img6, img7, img8;
+
+    @FXML
+    Button button1, button2, button3,button4,button5,button6,button7,button8, cartbutton;
+
+    @FXML
+    private Button logoutbutton;
+
+    @FXML
+    private AnchorPane Scenepane;
+
+    @FXML
+    private ScrollPane scrollPane;
+
+    @FXML
+    private Stage stage;
+
+    @FXML
+    private Scene scene;
+
+    @FXML
+    private Parent root = null;
+
+    FXMLLoader loader;
+
+    @FXML
+    WomenCheckoutController WomenCheckoutController = null;
+
+    static BeigeCap Bcap = new BeigeCap();
+    static MiniCoquette mcoq = new MiniCoquette();
+    static BeigeShoes Shoes = new BeigeShoes();
+    static Bucket cap = new Bucket();
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        // ============== Ordinary Beige Cap ==================//
+        Bcap.setProductName("Ordinary Beige Cap");
+        name1.setText(Bcap.getProductName());
+
+        Bcap.setProductPrice(250.00);
+        price1.setText(Double.toString(Bcap.getProductPrice()));
+
+        Bcap.setProductImage("images/cap.jpg");
+        Image beigeCap = new Image(Bcap.getProductImage());
+        img1.setImage(beigeCap);
+
+        // ============== Mini Coquette ==================//
+
+        mcoq.setProductName("Mini Coquette");
+        name2.setText(mcoq.getProductName());
+
+        mcoq.setProductPrice(699.00);
+        price2.setText(Double.toString(mcoq.getProductPrice()));
+
+        mcoq.setProductImage("images/beige dress.jpg");
+        Image MiniCoquette = new Image(mcoq.getProductImage());
+        img2.setImage(MiniCoquette);
+
+        // ============== Chunk Beige Shoes  ==================//
+        Shoes.setProductName("Chunk Beige Shoes");
+        name3.setText(Shoes.getProductName());
+
+        Shoes.setProductPrice(2699.00);
+        price3.setText(Double.toString(Shoes.getProductPrice()));
+
+        Shoes.setProductImage("images/beige shoes.jpg");
+        Image BeigeShoes = new Image(Shoes.getProductImage());
+        img3.setImage(BeigeShoes);
+
+        // ============== Bucket Cap  ==================//
+        cap.setProductName("Bucket Cap");
+        name4.setText(cap.getProductName());
+
+        cap.setProductPrice(300.00);
+        price4.setText(Double.toString(cap.getProductPrice()));
+
+        cap.setProductImage("images/Bucket.jpg");
+        Image Bucket = new Image(cap.getProductImage());
+        img4.setImage(Bucket);
+
+
+
+        try {
+            loader = new FXMLLoader(getClass().getResource("/view/Checkout.fxml"));
+            root = loader.load();
+        } catch (Exception e) {
+            System.out.println("Oops!, there is a problem at " + e);
+        }
+
+        // Clears all items in Checkout.fxml
+        WomenCheckoutController = loader.getController();
+        WomenCheckoutController.myvbox.getChildren().removeAll(WomenCheckoutController.myvbox.getChildren());
+    }
+
+
+
+    public void buy(ActionEvent event) throws IOException {
+
+        AlertMaker.showSimpleAlert("hello", "item added");
+
+        Button sourceButton = (Button) event.getSource();
+
+        // If addtocart button is pressed, set product status to true
+        if (sourceButton.equals(button1)) {
+            Bcap.setProductStatus(true);
+            WomenCheckoutController.addItem(WomenCheckoutController.pane1);
+        }
+
+        else if (sourceButton == button2) {
+            mcoq.setProductStatus(true);
+            WomenCheckoutController.addItem(WomenCheckoutController.pane2);
+        }
+
+        else if (sourceButton == button3) {
+            Shoes.setProductStatus(true);
+            WomenCheckoutController.addItem(WomenCheckoutController.pane3);
+        }
+
+        else if (sourceButton == button4) {
+            cap.setProductStatus(true);
+            WomenCheckoutController.addItem(WomenCheckoutController.pane4);
+        }
+
+    }
+
+    // Goes to Checkout.fxml
+    public void gotocart(ActionEvent event) throws IOException {
+
+        Scene scene = new Scene(root);
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    //Goes back to ChooseGender
+        public void switchToGenderPref(ActionEvent event) throws IOException {
+       root = FXMLLoader.load(getClass().getResource("/view/ChooseGender.fxml"));
+       stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+       scene = new Scene(root);
+       stage.setScene(scene);
+       stage.show();
+    }
+
+    //logs out the program
+    public void logout (ActionEvent event) {
+
+        Alert alert = new Alert(AlertType.CONFIRMATION);
+        alert.setTitle("Exit");
+        alert.setHeaderText("You are about to exit page!");
+        alert.setContentText("Do you wish to stop shopping?");
+
+        if (alert.showAndWait().get() == ButtonType.OK) {
+            stage = (Stage) Scenepane.getScene().getWindow();
+            System.out.println("You have successfully logged out");
+            stage.close();
+        } 
+    }
+}
